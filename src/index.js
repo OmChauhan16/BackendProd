6235247931544d00009c17dfea92de7749c05779
OmChauhan16 <omc5246@gmail.com>
2024-11-20 08:18:14 +0530

    user registered

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
 import { app } from "./app.js";
-// const app = express();
 dotenv.config()
 
 connectDB()
@@ -10,7 +8,7 @@ connectDB()
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running on PORT:${process.env.PORT}`);
             // console.log(app._router);
-            
+
         })
     })
     .catch((err) => {
